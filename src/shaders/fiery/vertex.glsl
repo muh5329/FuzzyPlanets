5a6d1d35b99a6690a4260f2f9ea426d0d834ec6e
Muneeb Haq <muh5329@gmail.com>
2024-10-10 23:34:50 -0400

    addit vUv varrying for shaddertoy texture mapping

--- src/shaders/fiery/vertex.glsl
+++ src/shaders/fiery/vertex.glsl
@@ -1,9 +1,12 @@
 
 varying vec3 vNormal;
 varying vec3 vPosition;
-
+varying vec2 vUv; 
 void main()
 {
+    vUv = uv ;
+
+    
     // Position
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
     gl_Position = projectionMatrix * viewMatrix * modelPosition;
