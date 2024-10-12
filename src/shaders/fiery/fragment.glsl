bca4fd00c1388ce6fa9345466a3756251fc86e81
Muneeb Haq <muh5329@gmail.com>
2024-10-11 23:55:37 -0400

    updates to fragment shader for shadertoy

--- src/shaders/fiery/fragment.glsl
+++ src/shaders/fiery/fragment.glsl
@@ -4,14 +4,21 @@ uniform vec3 uSunDirection;
 varying vec3 vNormal;
 varying vec3 vPosition;
 
+uniform float iGlobalTime;
+uniform sampler2D iChannel0;
+uniform sampler2D iChannel1;
+
+varying vec2 vUv;
+
+
 void main()
 {
     vec3 viewDirection = normalize(vPosition - cameraPosition);
     vec3 normal = normalize(vNormal);
     vec3 color = vec3(0.0);
 
-    
-
+    vec2 p = gl_FragCoord.xy / iResolution.xy;
+    vec2 p = -1.0 + 2.0 *vUv;
     // Final color
     gl_FragColor = vec4(color, alpha);
     #include <tonemapping_fragment>
